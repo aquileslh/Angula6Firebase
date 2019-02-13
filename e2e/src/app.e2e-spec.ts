26923fd5764e948433ae4aebac7e9b7d5b9142b9
aquileslh <aquileslazaroh@sistemashumanos.com>
2019-02-13 16:55:47 -0600

    Cambio de nombre de proyecto de firebase a tienda

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to firebase!');
+    expect(page.getParagraphText()).toEqual('Welcome to tienda!');
   });
 });
