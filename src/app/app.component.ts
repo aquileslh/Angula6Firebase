ebb4dfa332542102929d371aefd623ac5ecc4fa6
aquileslh <aquileslazaroh@sistemashumanos.com>
2019-02-19 14:48:23 -0600

    Se agrega las librerias para loader bar y toaster

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AngularFirestore,  AngularFirestoreCollection} from '@angular/fire/firestore';
+import { ToastrService } from 'ngx-toastr';
+import { NgProgressRef, NgProgress } from '@ngx-progressbar/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+
+  progressRef: NgProgressRef;
+
   private itemsCollection: AngularFirestoreCollection<any>;
   items: Observable<any[]>;
-  constructor(private afs: AngularFirestore) {
+  constructor(
+    private afs: AngularFirestore,
+    private toaster: ToastrService,
+    private progress: NgProgress
+  ) {
     this.itemsCollection = afs.collection<any>('tienda');
     this.itemsCollection.valueChanges().forEach(item => console.log(item));
 
   }
 
+  ngOnInit(): void {
+    this.progressRef = this.progress.ref('myProgress');
+    this.progressRef.start();
+  }
+
   addItem(item: any) {
     this.itemsCollection.add(item);
+  }
+
+  setPercentage() {
+    this.progressRef.inc(15);
+  }
+
+  showToaster() {
+    this.progressRef.complete();
+
+    this.toaster.success('In conclusion, there’s no blueprint when it comes to creating a folder-structure in Angular. The structure will change a lot depending on the project you’re working on. ', '', {positionClass: 'toast-top-center'});
   }
 
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { ToastrModule } from 'ngx-toastr';
+import { NgProgressModule } from '@ngx-progressbar/core';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,12 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase, 'adminSchool'),
-    AngularFirestoreModule
+    BrowserAnimationsModule, // required animations module
+    AngularFirestoreModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-center',
+    }), // ToastrModule added
+    NgProgressModule
   ],
   providers: [],
   bootstrap: [AppComponent]
